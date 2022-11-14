962c5c0e5b3f9a8aeef297a60b2fc8749c75d483
sylvia <sylviawang1102@gmail.com>
2022-11-14 15:34:44 +0800

    test4

---
+++ src/test/java/com/github/sylviawgx/CustomCalculatorTest.java
@@ -0,0 +1,15 @@
+package com.github.sylviawgx;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CustomCalculatorTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void add() {
+    }
+}
